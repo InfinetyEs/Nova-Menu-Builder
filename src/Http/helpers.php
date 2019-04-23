f76c2e478f050ec6aa97ce7c4ea07eca8d94c1bf
Kiko Seijo <kiko@sunnyface.com>
2019-04-23 21:37:03 +0200

    With this helper function and a blade, have reduce the database calls into half.

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -2,6 +2,17 @@
 
 use Infinety\MenuBuilder\Http\Models\Menu;
 
+if (!function_exists('getMenuBySlug')) {
+    function getMenuBySlug($slug)
+    {
+        return Menu::whereHas('items', function($query){
+            $query->with('children');
+            $query->where('enabled',1);
+            $query->orderBy('order', 'asc');
+        })->whereSlug($slug)->first();
+    }
+}
+
 if (!function_exists('menu_builder')) {
 
     /**
